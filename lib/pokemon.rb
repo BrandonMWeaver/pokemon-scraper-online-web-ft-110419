986a3d6f05e4fe701d00d14161c9ef4cb3a0cdb8
Brandon Weaver <a7xncob@gmail.com>
2019-12-11 22:39:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,7 +4,7 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
   
   def initialize(attributes)
-    attributes.each { |attribute, value| self.send("#{attribute}=", value) }
+    attributes.each { |key, value| self.send("#{key}=", value) }
   end
   
   def self.save(name, type, db)
