10a611347484ab4e78cdcd46700ce725c1cca53f
Brandon Weaver <a7xncob@gmail.com>
2019-12-11 22:38:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -32,7 +32,7 @@ class Pokemon
   end
   
   def self.new_from_db(row)
-    return self.new({id: row[0], name: row[1], type: row[2]})
+    return self.new(id: row[0], name: row[1], type: row[2])
   end
   
 end
